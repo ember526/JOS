a0e6d58d7ae92574e099c1966193b5a6b47f627e
15347109301@163.com <15347109301@163.com>
2019-03-06 19:16:03 +0800

    ""

---
+++ inc/pthread.h
@@ -0,0 +1,14 @@
+#ifndef JOS_INC_PTHREAD_H
+#define JOS_INC_PTHREAD_H
+
+typedef struct {
+	int pad;
+} pthread_attr_t;
+typedef unsigned long int pthread_t;
+
+
+int pthread_create(pthread_t * thread, const pthread_attr_t * attr,  void * (*start_routine)(void*), void * arg);
+int pthread_join(pthread_t thread, void **value_ptr);
+
+
+#endif /* !JOS_INC_PTHREAD_H */
---
+++ lib/pthread.c
@@ -0,0 +1,5 @@
+#include <inc/lib.h>
+
+int pthread_create(pthread_t * thread, const pthread_attr_t * attr,  void * (*start_routine)(void*), void * arg) {
+	return 0;
+}
